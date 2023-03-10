7caf2beeb21803c0c3e49566bc36b0df73d226eb
juck Zhong <guofeng.zhong@zoom.us>
2023-03-10 14:46:23 -0800

    test

--- main.cpp
+++ main.cpp
@@ -1,10 +1,21 @@
 #include <iostream>
+#include <map>
+#include <string>
+class TestSession
+{
+public:
+	TestSession(){}
+	int getTime() { return 1; }
+};
+
+typedef std::map<std::string, TestSession*>		sessionMap;
 
 int main(int argc, char* argv[])
 {
+	sessionMap testmap;
 	if (argc > 1)
 		return 1;
-
+	int test = testmap["1"]->getTime();
 	std::cout << "Hello World!" << std::endl;
 
 	return 0;
